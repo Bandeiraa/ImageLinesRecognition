05a2d129ccd0aacff7aefee2f1d740cb41febfb6
samuel <fsamuelsmartins@gmail.com>
2021-02-08 13:48:48 -0300

    encontrando borda da folha

--- chapter1.py
+++ chapter1.py
@@ -4,48 +4,51 @@ import cv2
 import pandas as pd
 import matplotlib.image as mpimg
 
+
 def get_lines(lines_in):
     if cv2.__version__ < '3.0':
         return lines_in[0]
     return [l[0] for l in lines_in]
 
+
 def empty(value):
     pass
 
+
 def process_lines(image_src):
     img = image_src
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret, thresh1 = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh1 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
     thresh1 = cv2.bitwise_not(thresh1)
 
-    edges = cv2.Canny(thresh1, threshold1=50, threshold2=200, apertureSize = 3)
+    edges = cv2.Canny(thresh1, threshold1=50, threshold2=200, apertureSize=3)
 
-    lines = cv2.HoughLinesP(thresh1, rho=1, theta=np.pi/180, threshold=50,
+    lines = cv2.HoughLinesP(thresh1, rho=1, theta=np.pi / 180, threshold=50,
                             minLineLength=50, maxLineGap=30)
 
     # resizedImage = cv2.resize(img, (300, 300))
     # l[0] - line; l[1] - angle
     for line in get_lines(lines):
         leftx, boty, rightx, topy = line
-        cv2.line(img, (leftx, boty), (rightx,topy), (0,0,255), 6)
+        cv2.line(img, (leftx, boty), (rightx, topy), (0, 0, 255), 6)
 
     cdstP = np.copy(lines)
 
     # merge lines
 
-    #------------------
+    # ------------------
     # prepare
     _lines = []
     for _line in get_lines(lines):
-        _lines.append([(_line[0], _line[1]),(_line[2], _line[3])])
+        _lines.append([(_line[0], _line[1]), (_line[2], _line[3])])
 
     # sort
     _lines_x = []
     _lines_y = []
     for line_i in _lines:
-        orientation_i = math.atan2((line_i[0][1]-line_i[1][1]),(line_i[0][0]-line_i[1][0]))
-        if (abs(math.degrees(orientation_i)) > 45) and abs(math.degrees(orientation_i)) < (90+45):
+        orientation_i = math.atan2((line_i[0][1] - line_i[1][1]), (line_i[0][0] - line_i[1][0]))
+        if (abs(math.degrees(orientation_i)) > 45) and abs(math.degrees(orientation_i)) < (90 + 45):
             _lines_y.append(line_i)
         else:
             _lines_x.append(line_i)
@@ -64,7 +67,7 @@ def process_lines(image_src):
     img_merged_lines = image_src
     blank_img = np.full(img_merged_lines.shape, 255, np.float32)
     for line in merged_lines_all:
-        cv2.line(blank_img, (line[0][0], line[0][1]), (line[1][0],line[1][1]), (0,0,255), 6)
+        cv2.line(blank_img, (line[0][0], line[0][1]), (line[1][0], line[1][1]), (0, 0, 255), 6)
 
     imageResized = cv2.resize(img_merged_lines, (400, 400))
     cv2.imwrite('prediction/lines_gray.jpg', gray)
@@ -74,9 +77,10 @@ def process_lines(image_src):
     cv2.imwrite('prediction/merged_lines.jpg', img_merged_lines)
     cv2.imwrite('prediction/whiteImage.jpg', blank_img)
 
-    #cv2.imshow("Image", blank_img)
+    # cv2.imshow("Image", blank_img)
 
     return merged_lines_all
+
 
 def merge_lines_pipeline_2(lines):
     super_lines_final = []
@@ -92,12 +96,13 @@ def merge_lines_pipeline_2(lines):
             for line2 in group:
                 if get_distance(line2, line) < min_distance_to_merge:
                     # check the angle between lines
-                    orientation_i = math.atan2((line[0][1]-line[1][1]),(line[0][0]-line[1][0]))
-                    orientation_j = math.atan2((line2[0][1]-line2[1][1]),(line2[0][0]-line2[1][0]))
+                    orientation_i = math.atan2((line[0][1] - line[1][1]), (line[0][0] - line[1][0]))
+                    orientation_j = math.atan2((line2[0][1] - line2[1][1]), (line2[0][0] - line2[1][0]))
 
-                    if int(abs(abs(math.degrees(orientation_i)) - abs(math.degrees(orientation_j)))) < min_angle_to_merge:
-                        #print("angles", orientation_i, orientation_j)
-                        #print(int(abs(orientation_i - orientation_j)))
+                    if int(abs(
+                            abs(math.degrees(orientation_i)) - abs(math.degrees(orientation_j)))) < min_angle_to_merge:
+                        # print("angles", orientation_i, orientation_j)
+                        # print(int(abs(orientation_i - orientation_j)))
                         group.append(line)
 
                         create_new_group = False
@@ -115,56 +120,58 @@ def merge_lines_pipeline_2(lines):
                 # check the distance between lines
                 if get_distance(line2, line) < min_distance_to_merge:
                     # check the angle between lines
-                    orientation_i = math.atan2((line[0][1]-line[1][1]),(line[0][0]-line[1][0]))
-                    orientation_j = math.atan2((line2[0][1]-line2[1][1]),(line2[0][0]-line2[1][0]))
+                    orientation_i = math.atan2((line[0][1] - line[1][1]), (line[0][0] - line[1][0]))
+                    orientation_j = math.atan2((line2[0][1] - line2[1][1]), (line2[0][0] - line2[1][0]))
 
-                    if int(abs(abs(math.degrees(orientation_i)) - abs(math.degrees(orientation_j)))) < min_angle_to_merge:
-                        #print("angles", orientation_i, orientation_j)
-                        #print(int(abs(orientation_i - orientation_j)))
+                    if int(abs(
+                            abs(math.degrees(orientation_i)) - abs(math.degrees(orientation_j)))) < min_angle_to_merge:
+                        # print("angles", orientation_i, orientation_j)
+                        # print(int(abs(orientation_i - orientation_j)))
 
                         new_group.append(line2)
 
                         # remove line from lines list
-                        #lines[idx] = False
+                        # lines[idx] = False
             # append new group
             super_lines.append(new_group)
-
 
     for group in super_lines:
         super_lines_final.append(merge_lines_segments1(group))
 
     return super_lines_final
 
+
 def merge_lines_segments1(lines, use_log=False):
-    if(len(lines) == 1):
+    if (len(lines) == 1):
         return lines[0]
 
     line_i = lines[0]
 
     # orientation
-    orientation_i = math.atan2((line_i[0][1]-line_i[1][1]),(line_i[0][0]-line_i[1][0]))
+    orientation_i = math.atan2((line_i[0][1] - line_i[1][1]), (line_i[0][0] - line_i[1][0]))
 
     points = []
     for line in lines:
         points.append(line[0])
         points.append(line[1])
 
-    if (abs(math.degrees(orientation_i)) > 45) and abs(math.degrees(orientation_i)) < (90+45):
+    if (abs(math.degrees(orientation_i)) > 45) and abs(math.degrees(orientation_i)) < (90 + 45):
 
-        #sort by y
+        # sort by y
         points = sorted(points, key=lambda point: point[1])
 
         if use_log:
             print("use y")
     else:
 
-        #sort by x
+        # sort by x
         points = sorted(points, key=lambda point: point[0])
 
         if use_log:
             print("use x")
 
-    return [points[0], points[len(points)-1]]
+    return [points[0], points[len(points) - 1]]
+
 
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html
 # https://stackoverflow.com/questions/32702075/what-would-be-the-fastest-way-to-find-the-maximum-of-all-possible-distances-betw
@@ -174,20 +181,22 @@ def lines_close(line1, line2):
     dist3 = math.hypot(line1[0][0] - line2[0][2], line1[0][0] - line2[0][3])
     dist4 = math.hypot(line1[0][2] - line2[0][2], line1[0][3] - line2[0][3])
 
-    if (min(dist1,dist2,dist3,dist4) < 100):
+    if (min(dist1, dist2, dist3, dist4) < 100):
         return True
     else:
         return False
 
-def lineMagnitude (x1, y1, x2, y2):
-    lineMagnitude = math.sqrt(math.pow((x2 - x1), 2)+ math.pow((y2 - y1), 2))
+
+def lineMagnitude(x1, y1, x2, y2):
+    lineMagnitude = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
     return lineMagnitude
 
-#Calc minimum distance from a point and a line segment (i.e. consecutive vertices in a polyline).
+
+# Calc minimum distance from a point and a line segment (i.e. consecutive vertices in a polyline).
 # https://nodedangles.wordpress.com/2010/05/16/measuring-distance-from-a-point-to-a-line-segment/
 # http://paulbourke.net/geometry/pointlineplane/
 def DistancePointLine(px, py, x1, y1, x2, y2):
-    #http://local.wasp.uwa.edu.au/~pbourke/geometry/pointline/source.vba
+    # http://local.wasp.uwa.edu.au/~pbourke/geometry/pointline/source.vba
     LineMag = lineMagnitude(x1, y1, x2, y2)
 
     if LineMag < 0.00000001:
@@ -198,8 +207,8 @@ def DistancePointLine(px, py, x1, y1, x2, y2):
     u = u1 / (LineMag * LineMag)
 
     if (u < 0.00001) or (u > 1):
-        #// closest point does not fall within the line segment, take the shorter distance
-        #// to an endpoint
+        # // closest point does not fall within the line segment, take the shorter distance
+        # // to an endpoint
         ix = lineMagnitude(px, py, x1, y1)
         iy = lineMagnitude(px, py, x2, y2)
         if ix > iy:
@@ -214,6 +223,7 @@ def DistancePointLine(px, py, x1, y1, x2, y2):
 
     return DistancePointLine
 
+
 def get_distance(line1, line2):
     dist1 = DistancePointLine(line1[0][0], line1[0][1],
                               line2[0][0], line2[0][1], line2[1][0], line2[1][1])
@@ -223,34 +233,36 @@ def get_distance(line1, line2):
                               line1[0][0], line1[0][1], line1[1][0], line1[1][1])
     dist4 = DistancePointLine(line2[1][0], line2[1][1],
                               line1[0][0], line1[0][1], line1[1][0], line1[1][1])
-    return min(dist1,dist2,dist3,dist4)
+    return min(dist1, dist2, dist3, dist4)
+
 
 if __name__ == '__main__':
     img = cv2.imread("Resources/Image.jpeg")
     a = process_lines(img)
-    df = {"x1":[],"y1":[],"x2":[],"y2":[]}
-    dfc = {"x1": [], "y1": [], "z1": [], "x2": [], "y2": [], "z2": [], "x3": [], "y3": [], "z3": [], "x4": [], "y4": [], "z4": []}
+    df = {"x1": [], "y1": [], "x2": [], "y2": []}
+    dfc = {"x1": [], "y1": [], "z1": [], "x2": [], "y2": [], "z2": [], "x3": [], "y3": [], "z3": [], "x4": [], "y4": [],
+           "z4": []}
     for i in a:
         p1 = i[0]
         p2 = i[1]
 
         offset = 100
-        #dist = ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
+        # dist = ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
         df["x1"].append(p1[0])
         df["y1"].append(p1[1])
         df["x2"].append(p2[0])
         df["y2"].append(p2[1])
-        #df["Dist"].append(dist)
+        # df["Dist"].append(dist)
 
         xVar = np.array([p1[0], 0, p1[1]])
         xVar2 = np.array([p2[0], 0, p2[1]])
         targetX = abs(xVar - xVar2)
         normalizedTarget = targetX / np.linalg.norm(targetX)
-        #print(normalizedTarget)
+        # print(normalizedTarget)
         result = np.cross(normalizedTarget, np.array([0, 1, 0])) * offset
-        #print("Resultado: ", result)
-        #print(p1[0] - result)
-        #print(result[0])
+        # print("Resultado: ", result)
+        # print(p1[0] - result)
+        # print(result[0])
 
         p1 = p1[0] - result
         p2 = p1[1] - result
@@ -270,21 +282,21 @@ if __name__ == '__main__':
         dfc["y4"].append(int(p4[1]))
         dfc["z4"].append(int(p4[2]))
 
-        #dfc["x1"].append(p1[0])
-        #dfc["y1"].append(p1[1])
-        #dfc["z1"].append(p1[2])
-        #dfc["x2"].append(p2[0])
-        #dfc["y2"].append(p2[1])
-        #dfc["z2"].append(p2[2])
-        #dfc["x3"].append(p3[0])
-        #dfc["y3"].append(p3[1])
-        #dfc["z3"].append(p3[2])
-        #dfc["x4"].append(p4[0])
-        #dfc["y4"].append(p4[1])
-        #dfc["z4"].append(p4[2])
+        # dfc["x1"].append(p1[0])
+        # dfc["y1"].append(p1[1])
+        # dfc["z1"].append(p1[2])
+        # dfc["x2"].append(p2[0])
+        # dfc["y2"].append(p2[1])
+        # dfc["z2"].append(p2[2])
+        # dfc["x3"].append(p3[0])
+        # dfc["y3"].append(p3[1])
+        # dfc["z3"].append(p3[2])
+        # dfc["x4"].append(p4[0])
+        # dfc["y4"].append(p4[1])
+        # dfc["z4"].append(p4[2])
 
     dfc = pd.DataFrame(dfc)
     df = pd.DataFrame(df)
     df.to_csv("Graph.csv", index=False)
-    dfc.to_csv("Grafo.csv", index=False, sep = ";")
+    dfc.to_csv("Grafo.csv", index=False, sep=";")
     cv2.waitKey(0)
